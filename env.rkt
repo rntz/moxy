dc2c703fe71f21c6e90d79ae3316d3b35a4a1f9a
Michael Arntzenius <daekharel@gmail.com>
2014-06-30 11:29:20 -0400

    move extension.rkt -> env.rkt

--- env.rkt
+++ env.rkt
@@ -17,12 +17,12 @@
 (define-syntax-rule (define-extension-point name empty join)
   (define name (make-extension-point (quote name) empty join)))
 
-;; Extension monoid values are immutable hashtables, mapping extension-points to
-;; their monoid values. If an extension-point is absent, it is the same as being
+;; Environments are immutable hashtables, mapping extension-points to their
+;; monoid values. If an extension-point is absent, it is the same as being
 ;; mapped to its empty value.
-(define ext-empty (hash))
+(define env-empty (hash))
 
-(define (ext-join-2 a b)
+(define (env-join-2 a b)
   (cond
     [(dict-empty? a) b]
     [(dict-empty? b) a]
@@ -35,16 +35,16 @@
                     (extension-point-empty k)))))
       a]))
 
-(define/match (ext-join . es)
-  [('()) ext-empty]
+(define/match (env-join . es)
+  [('()) env-empty]
   [(`(,a)) a]
-  [(`(,a ,b)) (ext-join-2 a b)]
-  [((cons a as)) (foldl ext-join-2 a as)])
+  [(`(,a ,b)) (env-join-2 a b)]
+  [((cons a as)) (foldl env-join-2 a as)])
 
-(define (ext-make ext-point value)
+(define (env-make ext-point value)
   (hash ext-point value))
 
-(define (ext-get ext ext-point)
+(define (env-get ext ext-point)
   (dict-ref ext ext-point (extension-point-empty ext-point)))
 
-(displayln "extension.rkt loaded")
+(displayln "env.rkt loaded")
