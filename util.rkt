c51c42dbcf4d8ef8ef2fdf4d87cf4eec726454b6
Michael Arntzenius <daekharel@gmail.com>
2014-07-04 14:25:22 -0400

    more values stuff

--- util.rkt
+++ util.rkt
@@ -2,7 +2,9 @@
 
 (provide
   const repr map_ flip partial nary unary
+  zip-with zip
   matches? lambda-rec eta
+  define-interface
   foldl1 reduce dict-union hash-unions)
 
 (define (const x) (lambda _ x))
@@ -16,6 +18,13 @@
 
 (define ((flip f) x y) (f y x))
 
+;; zips arbitrary sequences, not just lists
+(define (zip-with f xs ys)
+  (for/list ([x xs] [y ys]) (f x y)))
+
+(define (zip xs ys) (zip-with list xs ys))
+
+
 ;;; Some syntactic help
 (define-syntax-rule (matches? exp pat)
   (match exp [pat #t] [_ #f]))
@@ -25,6 +34,34 @@
 
 (define-syntax-rule (eta f) (lambda x (apply f x)))
 
+;;; Syntax for interfaces
+(define-syntax define-interface
+  (syntax-rules ()
+    [(define-interface iface-name parents method ...)
+      (begin
+        (define iface-name (interface parents method ...))
+        (define-methods iface-name method ...))]))
+
+(define-syntax define-methods
+  (syntax-rules ()
+    [(define-methods iface-name) (begin)]
+    [(define-methods iface-name method methods ...)
+      (begin
+        (define-method iface-name method)
+        (define-methods iface-name methods ...))]))
+
+(define-syntax define-method
+  (syntax-rules ()
+    [(define-method iface-name (method contract))
+      (define-method iface-name method)]
+    [(define-method iface-name method)
+      (define method
+        (let ([g (generic iface-name method)])
+          (lambda (object . args)
+            (send-generic object g . args))))]))
+
+
+;; Data structure manipulations
 ;; not sure I need this
 (define (foldl1 f xs)
   (match xs
--- values.rkt
+++ values.rkt
@@ -1,16 +1,62 @@
 #lang racket
 
 (require (for-syntax racket/syntax))    ;format-id
+(require racket/generic)                ;for gen:custom-write
 
 (require "util.rkt")
 
+
+;; Hashtable interface. Modelled on
+;; TODO: more functionality
+;; http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Strict.html
 (provide
-  new-tag tag-name tag-uid tag-arity
-  ann make-ann ann-tag ann-args
+  hash-empty? hash-count                ;re-exports
+  hash-empty hash-single
+  hash-has? hash-lookup hash-get hash-get-or-else
+  hash-put hash-put-with)
+
+(define (hash-empty) (hash))
+(define (hash-single k v) (hash k v))
+
+(define (hash-has? k h) (hash-has-key? h k))
+(define (hash-lookup k h)
+  (if (hash-has? k h) (just (hash-ref h k)) none))
+(define (hash-get k h)
+  (hash-get-or-else k h (lambda () (error "key not in hash"))))
+(define (hash-get-or-else k h or-else)
+  (if (procedure? or-else)
+    (hash-ref k h or-else)
+    (error "or-else argument to hash-get-or-else must be a procedure")))
+
+(define (hash-put k v h) (hash-set h k v))
+(define (hash-put-with k v h f)
+  (hash-put k (maybe v (partial f v) (hash-lookup k h)) h))
+
+
+;; Tag & annotated value interface
+(provide
+  new-tag tag-name tag-uid tag-arity tag-field-index
+  ann make-ann ann-tag ann-args ann-get-field
+  define-tag
   )
 
 ;; omit-define-syntaxes necessary to allow (define-match-expander ann) later
-(struct ann (tag args) #:prefab)
+(struct ann (tag args) #:transparent
+  #:methods gen:custom-write
+  ;; mode is #t for 'write, #f for 'display, or 0 or 1 (indicating quoting
+  ;; depth) for 'print.
+  [(define (write-proc self to-port mode)
+     (let* ([recur (match mode
+                    ['#t (lambda (x) (write x to-port))]
+                    ['#f (lambda (x) (display x to-port))]
+                    [(or '0 '1) (lambda (x) (print x to-port mode))])]
+            [tag (ann-tag self)]
+            [name (tag-name tag)])
+       ;; this completely ignores mode, because I just don't care.
+       (recur
+         (if (= 0 (tag-arity tag))
+           name
+           (cons name (vector->list (ann-args self)))))))])
 
 (struct tag (name uid arity field-map) #:prefab
   #:constructor-name make-tag)
@@ -20,7 +66,9 @@
     (make-immutable-hash (zip-with cons field-names (in-naturals 0)))))
 
 (define (tag-field-index tag field-name)
-  (dict-ref (tag-field-map tag) field-name))
+  ;; TODO: better error message on failure
+  (hash-get-or-else field-name (tag-field-map tag)
+    (lambda () "tag has no such field")))
 
 (define (make-ann tag . args)
   (if (= (tag-arity tag) (length args))
@@ -38,7 +86,7 @@
 ;; note the single n, versus double n in "ann".
 (define-syntax (define-tag stx)
   (with-syntax* ([(_ name fields ...) stx]
-                 [tag-name (format-id stx "tag-~a" #'name)])
+                 [tag-name (format-id stx "tag:~a" #'name)])
     (let ([field-list (syntax->datum #'(fields ...))])
       #`(begin
           (define tag-name (new-tag 'name '#,field-list))
@@ -61,7 +109,10 @@
               )))
       )))
 
+
 ;; Builtin tags.
+(provide tag:just just tag:none none maybe from-maybe)
+
 (define-tag just value)
 (define-tag none)
 
@@ -69,19 +120,3 @@
   (match v [(none) default] [(just x) (inject x)]))
 
 (define (from-maybe default v) (maybe v identity v))
-
-;; Hashtable interface. Modelled on
-;; http://hackage.haskell.org/package/containers-0.5.5.1/docs/Data-Map-Strict.html
-(provide hash-empty? hash-count)
-
-(define (hash-has? k h) (hash-has-key? h k))
-(define (hash-lookup k h)
-  (if (hash-has? k h) (make-ann tag-just (hash-ref h k)) (make-ann tag-none)))
-
-(define (hash-empty) (hash))
-(define (hash-single k v) (hash k v))
-
-(define (hash-get k h) (hash-ref k h (lambda () (error "key not in hash"))))
-(define (hash-put k v h) (hash-set h k v))
-(define (hash-put-with f k v h)
-  (hash-put k (maybe v (partial f v) (hash-lookup k h)) h))
