299c033029136f7e92ff9202f3732f5ee8452c42
Michael Arntzenius <daekharel@gmail.com>
2014-07-22 17:26:45 -0400

    more work on rewrite of parser

--- parse.rkt
+++ parse.rkt
@@ -20,11 +20,9 @@
     (lambda (loc msg) `(soft ,loc ,msg))
     (lambda (_ r) `(ok ,r))))
 
-;; (define (tag n . as) (apply <$> (partial list n) as))
 (define (keyword id) (expect (TID id)))
 (define (keysym id) (expect (TSYM id)))
 
-;;; Atoms.
 (define comma (keysym ","))
 (define dot (keysym "."))
 (define semi (keysym ";"))
@@ -47,18 +45,7 @@
 ;; A comma-separated-list, except leading and/or trailing commas are allowed.
 (define (p-listish p) (begin-sep-end-by p comma))
 
-;; Precedence levels, taken largely from Haskell (larger binds tighter than
-;; smaller, right binds tighter than left):
-;;
-;;   infixr 0 $
-;;   infixr 2 ||
-;;   infixr 3 &&
-;;   infixr 5 : ++
-;;   infixl 6 + -
-;;   infixl 7 * /
-;;   infixr 8 ^
-;;   infixl 10 function application f(x,y,z)
-
+
 ;; Problem: precedence not taken into account. "let" does not have same
 ;; precedence as function application. Is this a real problem?
 ;;
@@ -76,17 +63,14 @@
       (try-one-maybe
         (lambda (t) (hash-lookup t (env-get @exprs parse-env)))))
     ;; Run the parser we found in @exprs!
-    (lambda (parser) parser)))
+    (lambda (ext) (@expr-parser ext))))
 
 ;; Parses an expression.
-(define p-expr (eta (p-expr-at 10)))
-
-;; TODO: if, lambda, list, return, case, dict literals
-
 (define (p-expr-at prec)
   (>>= (p-prefix-expr-at prec)
-       (lambda (e) (p-infix-expr prec e))))
+    (lambda (e) (p-infix-expr prec e))))
 
+;; This doesn't use `prec', but maybe in future it will?
 (define (p-prefix-expr-at prec)
   (choice
     (<$> expr:lit (choice p-str p-num))
@@ -95,16 +79,50 @@
     ;; extensions from this?
     (<$> expr:var p-id)))
 
-(define (p-infix-expr prec left-expr)
-  ;; ask @infix-exprs what to do
+;; Note on infix precedence: Larger precedences bind tighter than smaller, and
+;; right-associative binds tighter than left-associative.
+
+;; Tries to parse an infix continuation for `left-expr' of precedence at least
+;; `prec' (i.e. binding at least as tightly as `prec').
+(define (p-infix-expr left-expr prec)
   ;; TODO: should we have a separate @infix-ops?
-  (>>= ask
-       (lambda (parse-env)
-         (try (try-one-maybe
-                (lambda (t) (hash-lookup t (env-get @infix-exprs parse-env))))))
-       (lambda (infix-expr)
-         (error "unimplemented")        ;FIXME
-         )))
+  (option left-expr
+    (>>= ask
+      (lambda (parse-env)
+        (try-one-maybe
+          ;; Look for an infix operator associated with the token `t' whose
+          ;; precedence is at least `prec' (i.e. as tight or tighter-binding as
+          ;; what we're currently looking for).
+          (lambda (t) (filter-maybe
+                   (hash-lookup t (env-get @infixes parse-env))
+                   (lambda (x) (<= prec (@infix-precedence x)))))))
+      (lambda (ext)
+        ;; Pass off to their parser
+        (@infix-parser ext left-expr))
+      ;; Try to keep parsing more operations afterward.
+      ;;
+      ;; Note: This handles left-associativity automatically. For
+      ;; right-associativity, the parser we got from @infixes should parse its
+      ;; right argument greedily, so that there's nothing left for us to parse
+      ;; here (at its infixity, anyway).
+      (lambda (x) (p-infix-expr x prec)))))
+
+(define p-expr (p-expr-at 0))
+
+
+;; -- The default/built-in parser extensions --
+;; TODO: if, lambda, list, return, case, dict literals
+
+;; Precedence levels, taken largely from Haskell:
+;;
+;;   infixr 0 $
+;;   infixr 2 ||
+;;   infixr 3 &&
+;;   infixr 5 : ++
+;;   infixl 6 + -
+;;   infixl 7 * /
+;;   infixr 8 ^
+;;   infixl 10 function application f(x,y,z)
 
 ;; (define (mk-funcalls func argses)
 ;;   (foldl (lambda (args f) (expr:call f args)) func argses))
